6538111bf8dec9f1336f6c56d5bfead873d2d45b
Toriality <pcrelier@hotmail.com>
2022-04-09 22:02:41 -0300

    Created colors.js

---
+++ src/styles/colors.js
@@ -0,0 +1,7 @@
+/*
+class Colors {
+    const PRIMARY_COLOR = "#ffc869";
+const SECONDARY_COLOR = "#fff";
+const BACKGROUND_CSS = `background: linear-gradient( 127deg, rgba(255, 248, 185, 0.8), rgba(0,255,0,0) 70.71%), linear-gradient(336deg, rgba(255, 185, 120, 0.8), rgba(0, 0, 255, 0) 70.71%) !important;`;
+}
+*/
