9d074b47926bd8f99dd34c3e027b5481809aa6a8
Toriality <pcrelier@hotmail.com>
2022-04-11 18:01:29 -0300

    Create colors object

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,7 +1,5 @@
-/*
-class Colors {
-    const PRIMARY_COLOR = "#ffc869";
-const SECONDARY_COLOR = "#fff";
-const BACKGROUND_CSS = `background: linear-gradient( 127deg, rgba(255, 248, 185, 0.8), rgba(0,255,0,0) 70.71%), linear-gradient(336deg, rgba(255, 185, 120, 0.8), rgba(0, 0, 255, 0) 70.71%) !important;`;
-}
-*/
+export const colors = {
+  primary: "#ffc869",
+  secondary: "#fff",
+  background_css: `background: linear-gradient( 127deg, rgba(255, 248, 185, 0.8), rgba(0,255,0,0) 70.71%), linear-gradient(336deg, rgba(255, 185, 120, 0.8), rgba(0, 0, 255, 0) 70.71%) !important;`,
+};
