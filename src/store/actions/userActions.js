3e945986d19aa4e4992365741830b081a78ecd96
Toriality <pcrelier@hotmail.com>
2022-04-12 18:02:17 -0300

    Set up sample folders and files

---
+++ src/store/actions/userActions.js
@@ -0,0 +1,17 @@
+import { GET_USERS, USERS_ERROR } from "../types";
+import axios from "axios";
+
+export const getUsers = () => async (dispatch) => {
+  try {
+    const res = await axios.get(`http://jsonplaceholder.typicode.com/users`);
+    dispatch({
+      type: GET_USERS,
+      payload: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: USERS_ERROR,
+      payload: console.log(e),
+    });
+  }
+};
