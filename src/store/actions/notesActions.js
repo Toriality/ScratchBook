843447ddcb4d593f5030b7632f5c3a6861fcfc37
Toriality <pcrelier@hotmail.com>
2022-04-15 13:50:37 -0300

    Replace localhost with herokuapp URL

--- src/store/actions/notesActions.js
+++ src/store/actions/notesActions.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // Get notes from the database
 export const getNotes = () => async (dispatch) => {
   try {
-    const res = await axios.get("http://localhost:5000/notes");
+    const res = await axios.get("https://my-scratch-book.herokuapp.com/notes");
     dispatch({
       type: type.GET_NOTES,
       payload: res.data,
@@ -20,7 +20,9 @@ export const getNotes = () => async (dispatch) => {
 // View a specific note
 export const viewNote = (id) => async (dispatch) => {
   try {
-    const res = await axios.get(`http://localhost:5000/notes/id/${id}`);
+    const res = await axios.get(
+      `https://my-scratch-book.herokuapp.com/notes/id/${id}`
+    );
     dispatch({
       type: type.VIEW_NOTE,
       payload: res.data,
@@ -36,7 +38,10 @@ export const viewNote = (id) => async (dispatch) => {
 // Create a new note and post it into the database
 export const postNote = (note) => async (dispatch) => {
   try {
-    const res = await axios.post("http://localhost:5000/notes", note);
+    const res = await axios.post(
+      "https://my-scratch-book.herokuapp.com/notes",
+      note
+    );
     dispatch({
       type: type.POST_NOTE,
       payload: res.data,
