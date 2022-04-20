ecc1e27ce0213b350d2c7e227f5eeaf7b65715a2
Toriality <pcrelier@hotmail.com>
2022-04-20 14:43:36 -0300

    Server merge

--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import notesReducers from "./notesReducers";
-import usersReducers from "./usersReducers";
-
-export default combineReducers({
-  notes: notesReducers,
-  users: usersReducers,
-});
--- src/styles/colors.js
+++
@@ -1,5 +0,0 @@
-export const colors = {
-  primary: "#ffc869",
-  secondary: "#fff",
-  background_css: `background: linear-gradient( 127deg, rgba(255, 248, 185, 0.8), rgba(0,255,0,0) 70.71%), linear-gradient(336deg, rgba(255, 185, 120, 0.8), rgba(0, 0, 255, 0) 70.71%) !important;`,
-};
